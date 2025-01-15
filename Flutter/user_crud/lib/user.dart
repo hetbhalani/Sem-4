26fa228b2b3d8c65be2a10a1c19a8c14b48bd194
hetbhalani <bhalanihet2006@gmail.com>
2025-01-15 23:10:39 +0530

    search

--- Flutter/user_crud/lib/user.dart
+++ Flutter/user_crud/lib/user.dart
@@ -1,27 +1,47 @@
-class User{
-  List<Map<String,dynamic>> users = [];
+class User {
+  List<Map<String, dynamic>> users = [];
 
-  void addUser({required userName,required email, required password}){
-    Map<String,dynamic> map = {};
-    map[userName] = userName;
-    map[email] = email;
-    map[password] = password;
+  void addUser({required userName, required email, required password}) {
+    Map<String, dynamic> map = {};
+    map['userName'] = userName;
+    map['email'] = email;
+    map['password'] = password;
     users.add(map);
   }
 
-  List<Map<String,dynamic>> readUser(){
+  List<Map<String, dynamic>> readUser() {
     return users;
   }
 
-  void updateUser({required userName,required email, required password, required id}){
-    Map<String,dynamic> map = {};
-    map[userName] = userName;
-    map[email] = email;
-    map[password] = password;
+  void updateUser(
+      {required userName, required email, required password, required id}) {
+    Map<String, dynamic> map = {};
+    map['userName'] = userName;
+    map['email'] = email;
+    map['password'] = password;
     users[id] = map;
   }
 
-  void deleteUser(id){
+  void deleteUser(id) {
     users.removeAt(id);
+  }
+
+  void searchUser({required data}) {
+    for (var elm in users) {
+      if (elm['userName']
+              .toString()
+              .toLowerCase()
+              .contains(data.toString().toLowerCase()) ||
+          elm['email']
+              .toString()
+              .toLowerCase()
+              .contains(data.toString().toLowerCase()) ||
+          elm['password']
+              .toString()
+              .toLowerCase()
+              .contains(data.toString().toLowerCase())) {
+        print("$elm['userName'] \n $elm['email'] \n $elm['password']");
+      }
+    }
   }
 }
