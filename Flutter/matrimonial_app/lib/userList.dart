38acc7af9ca5c1b3dc9f47cc837719960d3e0780
hetbhalani <bhalanihet2006@gmail.com>
2025-02-01 16:37:35 +0530

    search done

--- Flutter/matrimonial_app/lib/userList.dart
+++ Flutter/matrimonial_app/lib/userList.dart
@@ -11,6 +11,7 @@ class Userlist extends StatefulWidget {
 
   @override
   State<Userlist> createState() => _UserlistState();
+
 }
 
 class _UserlistState extends State<Userlist> {
@@ -24,6 +25,7 @@ class _UserlistState extends State<Userlist> {
   ];
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.w600);
+
   static const List<Widget> _widgetOptions = <Widget>[
     Text(
       'Home',
@@ -42,6 +44,26 @@ class _UserlistState extends State<Userlist> {
       style: optionStyle,
     ),
   ];
+
+  List<Map<String,dynamic>> searchedUsers = [];
+
+  void initState() {
+    super.initState();
+    searchedUsers = List.from(users);
+  }
+
+  void filterUsers(String query) {
+    setState(() {
+      if (query.isEmpty) {
+        searchedUsers = List.from(users);
+      } else {
+        searchedUsers = users.where((user) {
+          return user['name'].toLowerCase().contains(query.toLowerCase()) ||
+              user['city'].toLowerCase().contains(query.toLowerCase());
+        }).toList();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -66,125 +88,151 @@ class _UserlistState extends State<Userlist> {
         ),
         backgroundColor: Color.fromRGBO(255, 48, 48, 0.8),
       ),
-      body: ListView.builder(
-        itemCount: users.length,
-        itemBuilder: (context, index) {
-          return Card(
-            elevation: 5,
-            child: ListTile(
-              title: Row(
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          Image.asset(
-                            users[index]['gender'] ? 'assets/imgs/male.png' : 'assets/imgs/female.png',
-                            height: 40,
-                            width: 40,
-                          ),
-                          SizedBox(width: 10),
-                          Text(
-                            "${users[index]['name']}",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(50, 0, 0, 0),
-                        child: Row(
-                          children: [
-                            Text(
-                              "City:",
-                              style: TextStyle(fontWeight: FontWeight.w700),
-                            ),
-                            SizedBox(width: 8),
-                            Text(users[index]['city']),
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(50, 5, 0, 0),
-                        child: Row(
-                          children: [
-                            Text(
-                              "Phone:",
-                              style: TextStyle(fontWeight: FontWeight.w700),
-                            ),
-                            SizedBox(width: 10),
-                            Text(users[index]['phone']),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                  Spacer(),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      IconButton(
-                        icon: users[index]['isFav']
-                            ? Icon(Icons.favorite_rounded, color: Colors.pinkAccent)
-                            : Icon(Icons.favorite_border_rounded),
-                        iconSize: 25,
-                        onPressed: () {
-                          setState(() {
-                            if (!users[index]['isFav']) {
-                              FavUser.add(users[index]);
-                            } else {
-                              FavUser.removeWhere((user) => user['email'] == users[index]['email']);
-                            }
-                            print(FavUser);
-                            print("Fav btn pressed");
-                            users[index]['isFav'] = !users[index]['isFav'];
-                          });
-                        },
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.edit),
-                        iconSize: 25,
-                        color: Colors.blueAccent,
-                        onPressed: () {
-                          // Handle edit button press
-                          print("Edit button pressed");
-                        },
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.delete),
-                        iconSize: 25,
-                        color: Colors.red,
-                        onPressed: () {
-                          print("Delete button pressed");
-                          setState(() {
-                            users.removeAt(index);
-                          });
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content: Row(
-                                children: [
-                                  Icon(Icons.delete_outline_rounded, color: Colors.red),
-                                  SizedBox(width: 8),
-                                  Text("User Deleted successfully!"),
-                                ],
-                              ),
-                              backgroundColor: Colors.black87,
-                              behavior: SnackBarBehavior.floating,
-                              duration: Duration(seconds: 3),
-                            ),
-                          );
-                        },
-                      ),
-                    ],
-                  )
-                ],
-              ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+            child: TextFormField(
+              controller: search,
+              onChanged: (value) {
+                filterUsers(value);
+              },
+              decoration: InputDecoration(
+                  labelText: "Search",
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15)))),
             ),
-          );
-        },
+          ),
+          SizedBox(height: 10,),
+          Expanded(
+            child: ListView.builder(
+              itemCount: searchedUsers.length,
+              itemBuilder: (context, index) {
+                return Center(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    child: Card(
+                      elevation: 5,
+                      child: ListTile(
+                        title: Row(
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    Image.asset(
+                                      searchedUsers[index]['gender'] ? 'assets/imgs/male.png' : 'assets/imgs/female.png',
+                                      height: 35,
+                                      width: 35,
+                                    ),
+                                    SizedBox(width: 12),
+                                    Text(
+                                      "${searchedUsers[index]['name']}",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.fromLTRB(50, 0, 0, 0),
+                                  child: Row(
+                                    children: [
+                                      Text(
+                                        "City:",
+                                        style: TextStyle(fontWeight: FontWeight.w700),
+                                      ),
+                                      SizedBox(width: 8),
+                                      Text(searchedUsers[index]['city']),
+                                    ],
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.fromLTRB(50, 5, 0, 0),
+                                  child: Row(
+                                    children: [
+                                      Text(
+                                        "Phone:",
+                                        style: TextStyle(fontWeight: FontWeight.w700),
+                                      ),
+                                      SizedBox(width: 10),
+                                      Text(searchedUsers[index]['phone']),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Spacer(),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                IconButton(
+                                  icon: searchedUsers[index]['isFav']
+                                      ? Icon(Icons.favorite_rounded, color: Colors.pinkAccent)
+                                      : Icon(Icons.favorite_border_rounded),
+                                  iconSize: 25,
+                                  onPressed: () {
+                                    setState(() {
+                                      if (!users[index]['isFav']) {
+                                        FavUser.add(users[index]);
+                                      } else {
+                                        FavUser.removeWhere((user) => user['email'] == users[index]['email']);
+                                      }
+                                      print(FavUser);
+                                      print("Fav btn pressed");
+                                      users[index]['isFav'] = !users[index]['isFav'];
+                                    });
+                                  },
+                                ),
+                                IconButton(
+                                  icon: Icon(Icons.edit),
+                                  iconSize: 25,
+                                  color: Colors.blueAccent,
+                                  onPressed: () {
+                                    // Handle edit button press
+                                    print("Edit button pressed");
+                                  },
+                                ),
+                                IconButton(
+                                  icon: Icon(Icons.delete),
+                                  iconSize: 25,
+                                  color: Colors.red,
+                                  onPressed: () {
+                                    print("Delete button pressed");
+                                    setState(() {
+                                      users.removeAt(index);
+                                    });
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                        content: Row(
+                                          children: [
+                                            Icon(Icons.delete_outline_rounded, color: Colors.red),
+                                            SizedBox(width: 8),
+                                            Text("User Deleted successfully!"),
+                                          ],
+                                        ),
+                                        backgroundColor: Colors.black87,
+                                        behavior: SnackBarBehavior.floating,
+                                        duration: Duration(seconds: 3),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
       bottomNavigationBar: Container(
         decoration: BoxDecoration(
